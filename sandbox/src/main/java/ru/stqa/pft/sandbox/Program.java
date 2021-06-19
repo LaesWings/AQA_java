713937594fb4b39529f25a720ede2728d3f60252
Alexander <laeswings@bk.ru>
2021-06-19 22:36:02 +0300

    Сохраню пустой мейн

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Program.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Program.java
@@ -5,10 +5,6 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
-        System.out.println(2 + 2);
-        int l = 6;
-        int s = l * l;
-        System.out.println("Площадь квадрата со сторой " + l + " = " + s);
     }
 
 }
