8df193f9f102447b608ae08a878f371f9e41ee93
Alexander <laeswings@bk.ru>
2021-06-25 00:18:44 +0300

    coc

--- yandexmarket/src/test/java/Test1.java
+++ yandexmarket/src/test/java/Test1.java
@@ -1,19 +1,30 @@
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.List;
-import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class Test1 {
-    System.setProperty("webdriver.chrome.driver", "Z:chromedriver.exe");
-    WebDriver driver = new ChromeDriver();
-        @Test
-        public void simpleTest() {
-            driver.get("https://market.yandex.ru");
+    //System.setProperty("webdriver.chrome.driver", "Z:chromedriver.exe");
+    WebDriver driver;
+
+        @BeforeMethod
+        public void setTest() {
+            this.driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         }
 
+        @Test
+    public void simpleTest(){
+            driver.get("https://market.yandex.ru");
 
+        }
+
+        @AfterMethod
+    public void quitTest(){
+        driver.quit();
+        }
 }
